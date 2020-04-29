953a386c958ffe6fb973419072b2648721157e08
Jocelyn Gallardo <54326467+jocelyngallardo@users.noreply.github.com>
2020-04-29 16:38:01 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -61,7 +61,7 @@ def authorized():
             #save user data and set log in message
             session['github_token'] = (resp['access_token'],'')
             session['user_data'] = github.get('user').data
-            flash('You were successfully logged in as ' + session['user_data']['login'] + '.')
+            flash('You were successfully logged in as ' + session['user_data']['login'] + '.', 'message')
         except Exception as inst:
             #clear the session and give error message
             session.clear()
